14a4b3655c0d8f87d6b68e7fdddacc30ba61a821
Khalipha Thomas <70702235+kthomas94@users.noreply.github.com>
2020-10-07 10:00:07 -0400

    seeing what i did wrong with module 4

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -4,6 +4,18 @@
     <ul>
       <book-item v-for="book in books" :key="book.id" :book="book"></book-item>
     </ul>
+    <hr />
+    <h2>Filtered Books By Ownership</h2>
+    <select v-model="holding">
+      <option v-for="filter in filters" :key="filter.id">{{ filter }}</option>
+    </select>
+    <ul>
+      <book-item
+        v-for="book in filteredBooks"
+        :key="book.id"
+        :book="book"
+      ></book-item>
+    </ul>
     <br />
     <hr />
     <book-form @addBook="appendBook"></book-form>
@@ -13,6 +25,7 @@
 <script>
 import BookItem from "./BookItem";
 import BookForm from "./BookForm";
+import _ from "lodash";
 
 export default {
   name: "BookList",
@@ -25,23 +38,33 @@ export default {
           title: "Self-Reliance",
           author: "Ralph Waldo Emerson",
           finishedReading: true,
+          ownership: "borrowed",
         },
         {
           title: "American Gods",
           author: "Neil Gaiman",
           finishedReading: false,
+          ownership: "bought",
         },
         {
           title: "Amusing Ourselves to Death",
           author: "Neil Postman",
           finishedReading: true,
+          ownership: "borrowed",
         },
       ],
+      filters: ["bought", "borrowed"],
+      holding: "bought",
     };
   },
   components: {
     BookItem,
     BookForm,
+  },
+  computed: {
+    filteredBooks() {
+      return _.filter(this.books, ["ownership", this.holding]);
+    },
   },
   methods: {
     appendBook(bookData) {
@@ -49,6 +72,7 @@ export default {
         title: bookData.bookTitle,
         author: bookData.bookAuthor,
         finishedReading: bookData.finishedReading,
+        ownership: bookData.ownership,
       });
     },
   },
